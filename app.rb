19498d0a52598b6aa960e72581897ceb62e98a90
sammy-coder <sammymasib000@gmail.com>
2022-10-18 10:58:26 +0300

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,18 @@
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts [1, 2, 2].inspect 
+# #its equivalent to p
+# p [1, 2, 3]
+
+# print "hello world!"
+# print "hello world!"
+# print "hello world!"
+
+# puts "hello world!"
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello world!"
+p [1,2,3]
+print "Pass this test, please.";
